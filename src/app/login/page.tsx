f4716b0b95fe0de98daa9e5f508260398c0e22d7
Cyrus-0101 <cyrus_g@outlook.com>
2025-01-12 02:16:41 +0300

    feat: Created frontend

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Button } from '@/components/ui/button';
+import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/components'
+
+const Login = () => {
+  return (
+    <main className='h-dvh flex flex-col items-center gap-6 text-4xl p-4'>
+        <h1>Stevans Auto Parts Shop</h1>
+        
+        <Button asChild>
+            <LoginLink>Sign In</LoginLink>
+        </Button>
+    </main>
+  )
+}
+
+export default Login
