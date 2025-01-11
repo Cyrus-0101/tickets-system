498575148964e7b11deb36b8d6ad7b0ea41f6965
Cyrus-0101 <cyrus_g@outlook.com>
2025-01-11 22:55:28 +0300

    chore: Created not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,32 @@
+import Header from '@/components/Header'
+import Image from 'next/image'
+import React from 'react'
+
+export const metadata = {
+  title: "Page Not Found"
+}
+
+const NotFound = () => {
+  return (
+    <>
+      <Header />
+      <div className='px-2 w-full animate-appear'>
+          <div className="mx-auto py-4 flex flex-col justify-center items-center gap-4 mt-10">
+            <h2 className="text-2xl">Oops! Page Not Found</h2>
+            <Image
+              className="m-0 rounded-xl"
+              src="/images/404.png"
+              width={500}
+              height={500}
+              sizes='500px'
+              alt='Page Not Found'
+              priority={true}
+              title='Page Not Found'
+            />
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default NotFound
