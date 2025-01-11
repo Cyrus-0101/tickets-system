253d87d3de371b9a5979018e6f952f2c1355f27d
Cyrus-0101 <cyrus_g@outlook.com>
2025-01-11 23:06:25 +0300

    feat: Added support for dark and light theme

---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+
+export function ThemeToggleButton() {
+  const { setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return (
+    <Button variant="ghost" size="icon">
+      <span className="h-[1.2rem] w-[1.2rem]" />
+    </Button>
+  );
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    >
+      {resolvedTheme === "dark" ? (
+        <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+      ) : (
+        <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
